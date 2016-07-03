9d324b6c414a85b93adaf2436be94bde5a33a710
goesrrd <rdonahue@harris.com>
2016-07-03 17:18:57 -0400

    Update for termui refactoring

--- tui_addons.go
+++ tui_addons.go
@@ -3,7 +3,7 @@ package termui_addons
 
 import (
 	ui "github.com/gizak/termui"
-	rw "github.com/mattn/go-runewidth"
+	//rw "github.com/mattn/go-runewidth"
 	"strings"
 )
 
@@ -15,89 +15,127 @@ const (
 	BOTTOM DefaultPosition = iota
 )
 
-// ScrollingPar is like Par from termui.Par but is scrollable.
-type ScrollingPar struct {
-	ScrollingBlock
-	Text string
-}
-
-// NewScrollingPar returns a new *ScrollingPar with current theme.
-func NewScrollingPar() *ScrollingPar {
-	p := &ScrollingPar{}
-	p.Block = *ui.NewBlock()
-	p.FgColor = ui.Theme().ParTextFg
-	p.BgColor = ui.Theme().ParTextBg
-	return p
-}
-
-// Buffer implements Bufferer interface.
-func (p *ScrollingPar) Buffer() []ui.Point {
-	p.Runes = []rune(p.Text)
-	return p.ScrollingBlock.Buffer()
-}
-
-// ScrollingList is like List from termui.List but is scrollable.
-type ScrollingList struct {
-	ScrollingBlock
-	Items []string
-}
-
-// NewScrollingList returns a new *ScrollingList with current theme.
-func NewScrollingList() *ScrollingList {
-	l := &ScrollingList{}
-	l.Block = *ui.NewBlock()
-	l.FgColor = ui.Theme().ListItemFg
-	l.BgColor = ui.Theme().ListItemBg
-	return l
-}
-
-// Buffer implements Bufferer interface.
-func (l *ScrollingList) Buffer() []ui.Point {
-	l.Runes = []rune(strings.Join(l.Items, "\n"))
-	return l.ScrollingBlock.Buffer()
-}
-
-type ScrollingBlock struct {
-	ui.Block
-	Runes           []rune
-	FgColor         ui.Attribute
-	BgColor         ui.Attribute
+type Scroller struct {
+	block           *ui.Block
 	DefaultPosition DefaultPosition
 	startLine       int
 	inScroll        bool
 	linesInBuffer   int
 }
 
+func (s *Scroller) SetStartLine(line int) {
+	maxStartLine := s.calcStartFromEnd()
+	if line > maxStartLine {
+		s.startLine = maxStartLine
+	} else if line < 0 {
+		s.startLine = 0
+	} else {
+		s.startLine = line
+	}
+	if (s.startLine == 0 && s.DefaultPosition == TOP) || (s.startLine == maxStartLine && s.DefaultPosition == BOTTOM) {
+		s.inScroll = false
+	} else {
+		s.inScroll = true
+	}
+}
+
+func (s *Scroller) ScrollUp() {
+	s.SetStartLine(s.startLine - 1)
+}
+
+func (s *Scroller) ScrollDown() {
+	s.SetStartLine(s.startLine + 1)
+}
+
+func (s *Scroller) PageUp() {
+	s.SetStartLine(s.startLine - s.block.InnerHeight())
+}
+
+func (s *Scroller) PageDown() {
+	s.SetStartLine(s.startLine + s.block.InnerHeight())
+}
+
+func (s *Scroller) Home() {
+	s.SetStartLine(0)
+}
+
+func (s *Scroller) End() {
+	s.SetStartLine(s.linesInBuffer)
+}
+
+func (s *Scroller) ScrollToDefaultPosition() {
+	if s.DefaultPosition == TOP {
+		s.Home()
+	} else {
+		s.End()
+	}
+}
+
+func (s *Scroller) calcStartFromEnd() int {
+	start := s.linesInBuffer - s.block.InnerHeight()
+	if start < 0 {
+		start = 0
+	} else if start > 0 {
+		start += 1
+	}
+	return start
+}
+
+// ScrollingList is like List from termui.List but is scrollable.
+type ScrollingList struct {
+	ui.List
+	Scroller
+}
+
+// NewScrollingList returns a new *ScrollingList with current theme.
+func NewScrollingList() *ScrollingList {
+	l := &ScrollingList{}
+	l.List = *ui.NewList()
+	l.Scroller = Scroller{}
+	l.Scroller.block = &l.List.Block
+	return l
+}
+
+type lineAndSize struct {
+	cells []ui.Cell
+	size  int
+}
+
 // Buffer implements Bufferer interface.
-func (l *ScrollingBlock) Buffer() []ui.Point {
+func (l *ScrollingList) Buffer() ui.Buffer {
 
-	innerWidth := l.Width - l.PaddingLeft - l.PaddingRight - 3
-	startX := l.X + l.PaddingLeft + 1
+	innerBounds := l.List.Block.InnerBounds()
 
-	// Make rows
-	rows := make([]rowAndSize, 0)
-	runeCount := len(l.Runes)
-	if runeCount > 0 {
-		row := rowAndSize{make([]runeAndSize, 0), 0}
-		for i := 0; i < runeCount; i++ {
-			r := l.Runes[i]
-			w := rw.RuneWidth(r)
-			if r == '\n' || row.size+w > innerWidth {
-				rows = append(rows, row)
-				row = rowAndSize{make([]runeAndSize, 0), 0}
-			}
-			if r == '\n' {
+	// Get lines
+	var lines []lineAndSize
+	cells := ui.DefaultTxBuilder.Build(strings.Join(l.Items, "\n"), l.ItemFgColor, l.ItemBgColor)
+	if len(cells) > 0 {
+		line := lineAndSize{make([]ui.Cell, 0), 0}
+		for _, cell := range cells {
+			w := cell.Width()
+			ch := cell.Ch
+			if ch == '\n' {
+				lines = append(lines, line)
+				line = lineAndSize{make([]ui.Cell, 0), 0}
 				continue
 			}
-			row.rs = append(row.rs, runeAndSize{r, w})
-			row.size += w
+			if line.size+w > innerBounds.Dx() {
+				if l.Overflow == "wrap" {
+					lines = append(lines, line)
+					line = lineAndSize{make([]ui.Cell, 0), 0}
+				} else {
+					line.size += w
+					continue
+				}
+			}
+			line.cells = append(line.cells, cell)
+			line.size += w
 		}
-		rows = append(rows, row)
 	}
-	l.linesInBuffer = len(rows)
+	l.linesInBuffer = len(lines)
 
 	// Calculate which rows will be on screen
-	pageSize := l.PageSize()
+	pageSize := l.InnerHeight()
 	stopLine := 0
 	if l.inScroll || l.DefaultPosition == TOP {
 		if l.startLine > 0 {
@@ -115,118 +153,35 @@ func (l *ScrollingBlock) Buffer() []ui.Point {
 	}
 
 	// Render onscreen rows
-	yPos := l.Y + l.PaddingTop + 1
-	ps := l.Block.Buffer()
+	buf := l.Block.Buffer()
+	yPos := innerBounds.Min.Y
 	if l.startLine > 0 {
-		ps = l.appendMoreIndicator(startX, yPos, ps)
+		l.putMoreIndicator(buf, innerBounds.Min.X, yPos)
 		yPos++
 	}
 	for i := l.startLine; i < stopLine; i++ {
-		xPos := startX
-		for _, rs := range rows[i].rs {
-			pi := ui.Point{}
-			pi.X = xPos
-			pi.Y = yPos
-			pi.Ch = rs.r
-			pi.Bg = l.BgColor
-			pi.Fg = l.FgColor
-			ps = append(ps, pi)
-			xPos += rs.s
+		xPos := innerBounds.Min.X
+		for _, cell := range lines[i].cells {
+			buf.Set(xPos, yPos, cell)
+			xPos += cell.Width()
 		}
 		yPos++
 	}
 	if stopLine < l.linesInBuffer {
-		ps = l.appendMoreIndicator(startX, yPos, ps)
+		l.putMoreIndicator(buf, innerBounds.Min.X, yPos)
 	}
-	return ps
+
+	return buf
+
 }
 
-func (l *ScrollingBlock) SetStartLine(line int) {
-	maxStartLine := l.calcStartFromEnd()
-	if line > maxStartLine {
-		l.startLine = maxStartLine
-	} else if line < 0 {
-		l.startLine = 0
-	} else {
-		l.startLine = line
-	}
-	if (l.startLine == 0 && l.DefaultPosition == TOP) || (l.startLine == maxStartLine && l.DefaultPosition == BOTTOM) {
-		l.inScroll = false
-	} else {
-		l.inScroll = true
-	}
-}
-
-func (l *ScrollingBlock) ScrollUp() {
-	l.SetStartLine(l.startLine - 1)
-}
-
-func (l *ScrollingBlock) ScrollDown() {
-	l.SetStartLine(l.startLine + 1)
-}
-
-func (l *ScrollingBlock) PageUp() {
-	l.SetStartLine(l.startLine - l.PageSize())
-}
-
-func (l *ScrollingBlock) PageDown() {
-	l.SetStartLine(l.startLine + l.PageSize())
-}
-
-func (l *ScrollingBlock) Home() {
-	l.SetStartLine(0)
-}
-
-func (l *ScrollingBlock) End() {
-	l.SetStartLine(l.linesInBuffer)
-}
-
-func (l *ScrollingBlock) ScrollToDefaultPosition() {
-	if l.DefaultPosition == TOP {
-		l.Home()
-	} else {
-		l.End()
-	}
-}
-
-func (l *ScrollingBlock) LinesInBuffer() int {
-	return l.linesInBuffer
-}
-
-func (l *ScrollingBlock) PageSize() int {
-	return l.Height - l.PaddingTop - l.PaddingBottom - 2
-}
-
-func (l *ScrollingBlock) appendMoreIndicator(x int, y int, ps []ui.Point) []ui.Point {
+func (l *ScrollingList) putMoreIndicator(b ui.Buffer, x int, y int) {
 	dot := ([]rune("."))[0]
 	for i := 0; i < 3; i++ {
-		pi := ui.Point{}
-		pi.X = x + i
-		pi.Y = y
-		pi.Ch = dot
-		pi.Bg = l.BgColor
-		pi.Fg = l.FgColor
-		ps = append(ps, pi)
+		cell := ui.Cell{}
+		cell.Ch = dot
+		cell.Bg = l.ItemBgColor
+		cell.Fg = l.ItemFgColor
+		b.Set(x+i, y, cell)
 	}
-	return ps
-}
-
-func (l *ScrollingBlock) calcStartFromEnd() int {
-	start := l.linesInBuffer - l.PageSize()
-	if start < 0 {
-		start = 0
-	} else if start > 0 {
-		start += 1
-	}
-	return start
-}
-
-type rowAndSize struct {
-	rs   []runeAndSize
-	size int
-}
-
-type runeAndSize struct {
-	r rune
-	s int
 }
